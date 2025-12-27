ad7c4d5b2fd1bfd875e4d262c834690a3dc267c8
Le Tung Lam <letunglam1704@gmail.com>
2025-12-27 19:36:22 +0700

    update

--- src/main/java/com/javaweb/controller/client/HomePageController.java
+++ src/main/java/com/javaweb/controller/client/HomePageController.java
@@ -1,6 +1,7 @@
 package com.javaweb.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,11 @@ public class HomePageController {
     @GetMapping("/")
     public String homePage() {
         return "/client/homepage/show";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        return "/client/auth/register";
     }
 
 }
