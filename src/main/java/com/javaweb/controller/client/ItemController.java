4d47fb2abe995970a65fbcfbaca3043a0bbdc26d
Le Tung Lam <letunglam1704@gmail.com>
2026-01-14 14:08:07 +0700

    update

--- src/main/java/com/javaweb/controller/client/ItemController.java
+++ src/main/java/com/javaweb/controller/client/ItemController.java
@@ -2,6 +2,8 @@ package com.javaweb.controller.client;
 
 import com.javaweb.domain.Product;
 import com.javaweb.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,9 +35,18 @@ public class ItemController {
     }
 
     @PostMapping("/add-product-to-cart/{id}")
-    public String addProductToCart(@PathVariable long id) {
+    public String addProductToCart(@PathVariable long id, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        long productId = id;
+        String email = (String)session.getAttribute("email");
 
+        this.productService.hanldeAddProductToCart(email, productId);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/cart")
+    public String getCartPage(Model model) {
+        return "client/cart/show";
     }
 }
--- src/main/java/com/javaweb/service/ProductService.java
+++ src/main/java/com/javaweb/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.javaweb.service;
 
 
+import com.javaweb.domain.Cart;
+import com.javaweb.domain.CartDetail;
 import com.javaweb.domain.Product;
+import com.javaweb.domain.User;
 import com.javaweb.repository.CartDetailRepository;
 import com.javaweb.repository.CartRepository;
 import com.javaweb.repository.ProductRepository;
@@ -14,36 +17,59 @@ import java.util.Optional;
 public class ProductService {
     private final CartRepository cartRepository;
     private final CartDetailRepository cartDetailRepository;
-
+    private final UserService userService;
     private final ProductRepository productRepository;
-    public ProductService(CartRepository cartRepository, CartDetailRepository cartDetailRepository, ProductRepository productRepository) {
+
+    public ProductService(CartRepository cartRepository, CartDetailRepository cartDetailRepository, UserService userService, ProductRepository productRepository) {
         this.cartRepository = cartRepository;
         this.cartDetailRepository = cartDetailRepository;
+        this.userService = userService;
         this.productRepository = productRepository;
     }
 
-    public List<Product> fetchAllProducts()
-    {
+    public List<Product> fetchAllProducts() {
         return productRepository.findAll();
     }
 
-    public void handleSaveProduct(Product product)
-    {
+    public void handleSaveProduct(Product product) {
         this.productRepository.save(product);
     }
 
-    public Optional<Product> fetchProductById(long id)
-    {
+    public Optional<Product> fetchProductById(long id) {
         return this.productRepository.findById(id);
     }
 
-    public void deleteProductById(long id)
-    {
+    public void deleteProductById(long id) {
         this.productRepository.deleteById(id);
     }
 
-    public void hanldeAddProductToCart(){
-        
+    public void hanldeAddProductToCart(String email, long productId) {
+        //check đã có user hay chưa
+        User user = this.userService.getUserByEmail(email);
+        if (user != null) {
+            Cart cart = this.cartRepository.findByUser(user);
+            if (cart == null) {
+                //tao moi cart
+                Cart otherCart = new Cart();
+                otherCart.setUser(user);
+                otherCart.setSum(1);
+                cart = this.cartRepository.save(otherCart);
+            }
+
+            //save cartDetail
+            Optional<Product> p = this.productRepository.findById(productId);
+
+            if (p.isPresent()) {
+                Product realProduct = p.get();
+                CartDetail cd = new CartDetail();
+                cd.setCart(cart);
+                cd.setProduct(realProduct);
+                cd.setPrice(realProduct.getPrice());
+                cd.setQuantity(1);
+                this.cartDetailRepository.save(cd);
+            }
+
+        }
     }
 
 
